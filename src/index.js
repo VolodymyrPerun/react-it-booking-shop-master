fe98b06ba02f25203c1ced01f75fa996cba550ad
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2019-12-02 22:29:21 +0200

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import {Provider} from 'react-redux';
 import App from './containers/App';
 import 'semantic-ui-css/semantic.min.css'
